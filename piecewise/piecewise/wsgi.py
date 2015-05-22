ef9c09e6d75d6d8548f254da999b5cff34d34c9b
David Winslow <cdwinslow@gmail.com>
2015-05-21 20:17:27 -0400

    Add logging

--- piecewise/piecewise/wsgi.py
+++ piecewise/piecewise/wsgi.py
@@ -5,6 +5,12 @@ from piecewise.config import parse_date
 from piecewise.query import query
 from sqlalchemy import create_engine, select, MetaData, Table, Column, String, Integer
 app = Flask(__name__)
+if not app.debug:
+    import logging
+    import logging.handlers
+    handler = logging.handlers.RotatingFileHandler("/var/log/piecewise/wsgi.log", maxBytes = 10 * 1000 * 1000, backupCount = 5)
+    handler.setLevel(logging.WARNING)
+    app.logger.addHandler(handler)
 
 import json
 import piecewise.config
@@ -16,37 +22,31 @@ def hello_world():
 
 @app.route('/list')
 def list_records():
-    try:
-        resolution = request.args.get('res', 60, type=int)
-        after = request.args.get('after', parse_date('Jan 1 2010 00:00:00'))
-        before = request.args.get('before', parse_date('Feb 1 2010 00:00:00'))
-        results = general_query(config,
-                [AverageRTT],
-                bins = { 'time_slices' : resolution },
-                filters = { 'time_slices' : (after, before) })
-        results = { 'success': True, 'results': [list(r) for r in results] }
-        return (dumps(results), None, { 'Content-type': 'application/json' })
-    except Exception, e:
-        return repr(e)
+    resolution = request.args.get('res', 60, type=int)
+    after = request.args.get('after', parse_date('Jan 1 2010 00:00:00'))
+    before = request.args.get('before', parse_date('Feb 1 2010 00:00:00'))
+    results = general_query(config,
+            [AverageRTT],
+            bins = { 'time_slices' : resolution },
+            filters = { 'time_slices' : (after, before) })
+    results = { 'success': True, 'results': [list(r) for r in results] }
+    return (dumps(results), None, { 'Content-type': 'application/json' })
 
 @app.route('/cells')
 def map_records():
-    try:
-        resolution = request.args.get('res', 1, type=float)
-        ll = request.args.get('ll', "-90,-180")
-        ur = request.args.get('ur', "90,180")
-        minx, miny = tuple(float(s) for s in ll.split(",", 1))
-        maxx, maxy = tuple(float(s) for s in ur.split(",", 1))
+    resolution = request.args.get('res', 1, type=float)
+    ll = request.args.get('ll', "-90,-180")
+    ur = request.args.get('ur', "90,180")
+    minx, miny = tuple(float(s) for s in ll.split(",", 1))
+    maxx, maxy = tuple(float(s) for s in ur.split(",", 1))
 
-        results = general_query(config, 
-                [AverageRTT],
-                bins = { 'spatial_grid' : resolution },
-                filters =  { 'spatial_grid' : (minx, miny, maxx, maxy) })
+    results = general_query(config, 
+            [AverageRTT],
+            bins = { 'spatial_grid' : resolution },
+            filters =  { 'spatial_grid' : (minx, miny, maxx, maxy) })
 
-        results = { 'success': True, 'results': [list(r) for r in results] }
-        return (dumps(results), None, { 'Content-type': 'application/json' })
-    except Exception, e:
-        return repr(e)
+    results = { 'success': True, 'results': [list(r) for r in results] }
+    return (dumps(results), None, { 'Content-type': 'application/json' })
 
 if __name__ == '__main__': 
     app.run(debug=True)
