5bf22e668ef6a8440ae013156e553f56b503109b
Nathan Kinkade <nath@nkinka.de>
2015-08-06 17:04:04 -0400

    Attempt to make median functions ignore NULL values.

--- piecewise/setup.sql
+++ piecewise/setup.sql
@@ -8,6 +8,7 @@ $$
    FROM (
      SELECT val
      FROM unnest($1) val
+     WHERE val IS NOT NULL
      ORDER BY 1
      LIMIT  2 - MOD(array_upper($1, 1), 2)
      OFFSET CEIL(array_upper($1, 1) / 2.0) - 1
