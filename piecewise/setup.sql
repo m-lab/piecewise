8f82c1fa862e091ce52466224769f93875c84c3d
Nathan Kinkade <nath@nkinka.de>
2015-08-07 11:12:21 -0400

    A new ffunc median function which works and takes into account NULL values in the arrays (from Aggregate_Median page on postresql.org wiki).

--- piecewise/setup.sql
+++ piecewise/setup.sql
@@ -1,20 +1,26 @@
 CREATE EXTENSION postgis;
 CREATE EXTENSION btree_gist;
 
-CREATE OR REPLACE FUNCTION _final_median(float[])
-   RETURNS float AS
-$$
-   SELECT AVG(val)
-   FROM (
-     SELECT val
-     FROM unnest($1) val
-     WHERE val IS NOT NULL
-     ORDER BY 1
-     LIMIT  2 - MOD(array_upper($1, 1), 2)
-     OFFSET CEIL(array_upper($1, 1) / 2.0) - 1
-   ) sub;
-$$
-LANGUAGE 'sql' IMMUTABLE;
+CREATE OR REPLACE FUNCTION _final_median(float[]) RETURNS float8 AS $$
+  WITH q AS
+  (
+    SELECT val
+    FROM unnest($1) val
+      WHERE VAL IS NOT NULL
+      ORDER BY 1
+  ),
+  cnt AS
+  (
+    SELECT COUNT(*) AS c FROM q
+  )
+  SELECT AVG(val)::float8
+  FROM
+  (
+    SELECT val FROM q
+    LIMIT  2 - MOD((SELECT c FROM cnt), 2)
+    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1,0)
+  ) q2;
+$$ LANGUAGE SQL IMMUTABLE;
  
 CREATE AGGREGATE median(float) (
   SFUNC=array_append,
