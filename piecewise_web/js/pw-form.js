10afd68defa561fb538e95b31eff7b7404e96089
critzo <critzo@opentechinstitute.org>
2016-12-20 14:27:50 -0500

    Import angular app (#99)
    * changed branch for testing feature
    
    * correcting postgres create issue in ansible
    
    * switched from sudo to become in ansible playbooks
    
    * added line in system tasks- diagnosing playbook error
    
    * testing changes to ansible to create db
    
    * corrected become_user name
    
    * added ansible command to create self-signed cert
    
    * ssl directory needs to already exist. Add /etc/nginx/ssl and updated config
    
    * added ansible line to explicitly create /etc/nginx/ssl directory
    
    * working out the proper syntax for openssl to gen self-signed cert
    
    * updated git ignore to not include hosts file for ansible
    
    * updated ssl cert gen line and nginx config with my VM's IP
    
    * enable nginx default site
    
    * imported speed.measurementlab.net app files
    
    * adds uwsgi and nginx config lines to serve updated test from /app
    
    * corrects paths for angular app resources inside /app
    
    * added two playbooks, allowing for website files and python files to be updated independently
    
    * added two playbooks, allowing for website files and python files to be updated independently
    
    * corrected path for angular template
    
    * Test integrating PW form directly into Angular test template
    
    * first attempt to integrate piecewise js form submission functions into measure.js angular app.
    
    * corrected issue with JS for loop, undeclared var
    
    * declared var resultString
    
    * placement of html form in template was incorrect?
    
    * corrected variable name for timeStarted
    
    * removed form validation req. for data acknowledgement since you can't run the test without checking it
    
    * testing form submission after removal of blank elements
    
    * test modification to form validation function to remove un-needed fields
    
    * test modification to form validation function to remove un-needed fields
    
    * test modification to form validation function to remove un-needed fields
    
    * renamed demo-human checkbox to demoHuman
    
    * addition to jquery serialize call to remove unwanted values
    
    * addition to jquery serialize call to remove unwanted values
    
    * corrected syntax error
    
    * corrected syntax error
    
    * After this commit (or the prior commit), the original piecewise web form has been integrated with the Angular test in /app - with the exception of the isp_user_other field. Noting the progress here and will likely leave the app in a runable state from this subdirectory, while also integrating a copy of it on the main page to fully replace the old NDT_js version.
    
    * updated branch name to import-angular-app to test
    
    * added TO DO comment for making Nginx
    'server_name' IP/hostname a parameter instead of hardcoded

---
+++ piecewise_web/js/pw-form.js
@@ -0,0 +1,82 @@
+// checks for geolocation and adds coords to form elements upon consent
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function success(position) {
+
+  document.getElementById('latitude').value = position.coords.latitude;
+  document.getElementById('longitude').value = position.coords.longitude;
+
+  var xhr = new XMLHttpRequest(),
+  currentLocationURL = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&zoom=18&addressdetails=1";
+
+  var currentLoc;
+  xhr.open('GET', currentLocationURL, true);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        currentLoc = JSON.parse(xhr.responseText);
+        console.log("Location received");
+
+        // currentLocText.text(currentLoc.address.road + currentLoc.address.neighbourhood + currentLoc.address.suburb + currentLoc.address.city + currentLoc.address.state);
+        $('#mobile-container').append('<div id="mobile-approx-loc"></div>')
+        $('#approx-loc, #mobile-approx-loc').append("<p>Searching from:</p><p>" + currentLoc.address.road + ", " + currentLoc.address.city + ", " + currentLoc.address.state + "</p>");
+      } else {
+        console.log('Location lookup failed');
+      }
+    }
+  };
+}
+
+function error(error) {
+  document.getElementById('msg').innerHTML = 'ERROR(' + error.code + '): ' + error.message;
+}
+
+function submitExtraData() {
+  if (validateExtraDataForm()) {
+    var formData = $($("#collector")[0].elements).not("#other,#demoHuman").serialize();
+    $.ajax({
+      method: 'GET',
+      url: $('#collector').attr('action'),
+      data: formData,
+      statusCode: {
+        201: function() {
+          console.log('Data submitted successfully.');
+        }
+      },
+      error: function(jqXHR, status, msg) {
+        console.log('Something went wrong: ' + status + ' ' + msg);
+      }
+    });
+  }
+}
+
+function validateExtraDataForm() {
+  if ( $('#isp_user option:selected').val() == 'default' ) {
+    return false;
+  } else if ( $('#isp_user option:selected').val() == 'other' ) {
+    $('#isp_user_text').toggle(true);
+  } else {
+    $('#isp_user_text').toggle(false);
+  }
+  if ( $('#connection_type option:selected').val() == 'default' ) {
+    return false;
+  }
+  if ( $('#cost_of_service option:selected').val() == 'default' ) {
+    return false;
+  }
+  return true;
+}
+
+function showOtherIspBox(val) {
+  var element=document.getElementById('isp_user');
+  if(val=='other') {
+    $('#isp_user_text').show();
+  }
+  else {
+    $('#isp_user_text').hide();
+  }
+}
