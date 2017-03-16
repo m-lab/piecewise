71f57108cbbca8fa5d77751bf681f3bcdc0b2d94
Chris Ritzo <critzo@opentechinstitute.org>
2017-03-16 15:46:25 -0400

    moved static files into Ansible templates

---
+++ ansible/templates/map_center.js
@@ -0,0 +1,6 @@
+{% set map_center_lat = (map_coordinate_southwest.lat + map_coordinate_northeast.lat)/2 %}
+{% set map_center_lon = (map_coordinate_southwest.lon + map_coordinate_northeast.lon)/2 %}
+{% set map_center -%}
+	{{ map_center_lat|replace("(","") }}, {{ map_center_lon|replace(")","") }}
+{%- endset %}
+var center = [{{ map_center }}];
