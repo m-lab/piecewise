c323c9e8826933b37b06c0b2e296a38da738259a
Rae <rae.rudie@gmail.com>
2019-12-20 15:46:15 -0500

    refactor and hook up map to ndt test

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,9 @@
 // import $ from 'jquery-ui';
 import css from '../css/main.css';
 import 'bootstrap';
-import './form.js';
 import './map.js';
-import './mlab.js';
+import './script.js';
+import './ndt-browser-client.js';
+import './ndt-wrapper.js';
+import './ndt.js'
+import './ndt7-core.js'
