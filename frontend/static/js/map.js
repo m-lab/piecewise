a3e7aa5b2179d4c6841f388fd0acffee5a502b02
Chris Ritzo <critzo@measurementlab.net>
2020-03-19 12:00:25 -0400

    moved scroll/zoom disabling to different part of the file.

--- frontend/static/js/map.js
+++ frontend/static/js/map.js
@@ -127,16 +127,6 @@ if (!!consentForm && !!mapContainer) {
     //  });
     }
 
-    // Disable drag and zoom handlers.
-    map.dragging.disable();
-    map.touchZoom.disable();
-    map.doubleClickZoom.disable();
-    map.scrollWheelZoom.disable();
-    map.keyboard.disable();
-
-    // Disable tap handler, if present.
-    if (map.tap) map.tap.disable();
-
     surveyForm.addEventListener('submit', logSubmit);
 
     function logSubmit(event) {
@@ -196,6 +186,16 @@ if (!!consentForm && !!mapContainer) {
       //      .addTo(map);
       //  });
       //}
+
+      // Disable drag and zoom handlers.
+      map.dragging.disable();
+      map.touchZoom.disable();
+      map.doubleClickZoom.disable();
+      map.scrollWheelZoom.disable();
+      map.keyboard.disable();
+
+      // Disable tap handler, if present.
+      if (map.tap) map.tap.disable();
 
       results.children[0].classList.add('pos-fixed');
       results.children[0].classList.add('bg-white');
