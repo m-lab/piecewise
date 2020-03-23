564549356aa8e9ed4297a8a4e67dbaaa0d8324ac
Chris Ritzo <critzo@measurementlab.net>
2020-03-23 16:20:00 -0400

    modified placement of js code to get addtl metadata

--- frontend/static/js/ndt-d3.js
+++ frontend/static/js/ndt-d3.js
@@ -162,6 +162,35 @@ NDTmeter.prototype.onfinish = function (passed_results) {
     'MinRTT': 'Latency'
   };
 
+  function httpGet(theUrl)  {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  };
+
+  var getip;
+  getip = httpGet('https://ipinfo.io/json');
+
+  var obj = JSON.parse(getip);
+  var ip = obj.ip;
+  var city = obj.city;
+  var region = obj.region;
+  var country = obj.country;
+  var ip_location = obj.loc;
+  var asn = obj.org;
+  var zipcode = obj.postal;
+  var timezone = obj.timezone;
+  
+  document.getElementById('client_ip').value = ip;
+  document.getElementById('client_city').value = city;
+  document.getElementById('client_region').value = region;
+  document.getElementById('client_country').value = country;
+  document.getElementById('client_ipinfo_loc').value = ip_location;
+  document.getElementById('client_asn').value = asn;
+  document.getElementById('client_zipcode').value = zipcode;
+  document.getElementById('client_timezone').value = timezone;
+
   document.getElementById('bigquery_key').value =
       passed_results['StartTimeUsec'] + window.ndtServerIp
 
--- frontend/static/js/ndt-server.js
+++ frontend/static/js/ndt-server.js
@@ -110,28 +110,6 @@ function success(position) {
 			}
 		}
 	};
-	
-	var getip;
-	getip = httpGet('https://ipinfo.io/json');
-
-	var obj = JSON.parse(getip);
-	var ip = obj.ip;
-	var city = obj.city;
-	var region = obj.region;
-	var country = obj.country;
-	var ip_location = obj.loc;
-	var asn = obj.org;
-	var zipcode = obj.postal;
-	var timezone = obj.timezone;
-	console.log(obj);
-	document.getElementById('client_ip').value = ip;
-	document.getElementById('client_city').value = city;
-	document.getElementById('client_region').value = region;
-	document.getElementById('client_country').value = country;
-	document.getElementById('client_ipinfo_loc').value = ip_location;
-	document.getElementById('client_asn').value = asn;
-	document.getElementById('client_zipcode').value = zipcode;
-	document.getElementById('client_timezone').value = timezone;
 }
 
 function error(error) {
@@ -156,10 +134,4 @@ function getNdtServer() {
 			}
 		}
 	};
-};
-function httpGet(url)  {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, false ); // false for synchronous request
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
 };
