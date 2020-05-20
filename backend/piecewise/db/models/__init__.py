bed10c861b31786e765b6b0af19df8375b240107
Rae <rae.rudie@gmail.com>
2020-05-20 18:21:26 -0400

    restructuring and deleting extraneous files for koa merge

--- backend/piecewise/db/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import Base
-from .submission import Submission
-from .user import User
--- frontend/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-envsubst '$PIECEWISE_BACKEND_URL' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
-
-cp /usr/share/nginx/html/main.js /tmp/template.js
-
-envsubst '$PIECEWISE_BACKEND_URL' < /tmp/template.js > /usr/share/nginx/html/main.js
-
-exec nginx -g 'daemon off;'
