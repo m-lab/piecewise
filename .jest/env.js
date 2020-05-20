facab1684467e80ae64c36da6c9a7a24f13fcb33
Rae <rae.rudie@gmail.com>
2020-05-20 19:09:47 -0400

    add jest case for mapbox

--- .jest/env.js
+++ .jest/env.js
@@ -1,3 +1,10 @@
 import 'babel-polyfill';
 process.env.PIECEWISE_ADMIN_PASSWORD = 'areallylonggoodpassword';
 process.env.PIECEWISE_SECRETS = 'secret1,secret2';
+
+if (typeof window.URL.createObjectURL === 'undefined') {
+  window.URL.createObjectURL = () => {
+    // Do nothing
+    // Mock this function for mapbox-gl to work
+  };
+}
