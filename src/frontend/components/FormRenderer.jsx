0e6d360deede5b685007faa5efb6ee5be8282c86
Rae <rae.rudie@gmail.com>
2020-05-21 11:33:37 -0400

    working on babel transpile for mapbox

--- src/frontend/components/FormRenderer.jsx
+++ src/frontend/components/FormRenderer.jsx
@@ -2,35 +2,42 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const FormRenderer = props => {
+  const [ form, setForm ] = React.useState(null);
+  const formContainer = React.useRef(null);
+
   useEffect(() => {
-    import('formeo')
-      .then(({ FormeoRenderer }) => {
-        const container = document.querySelector('.formeo-renderer');
-        const options = {
-          editorContainer: container,
-          style: 'https://draggable.github.io/formeo/assets/css/formeo.min.css',
-          debug: true,
-        };
-        if (props.onSave) {
-          options.events = { onSave: props.onSave };
-        }
-        const renderer = new FormeoRenderer(options);
-        console.log('props: ', props);
-        props.onLoad().then(res => {
-          console.log('res.data: ', res.data);
-          renderer.render(res.data);
-          console.log('rendered!');
-          return;
+    const initializeForm = ({ setForm, formContainer }) => {
+      import('formeo')
+        .then(({ FormeoRenderer }) => {
+          const options = {
+            renderContainer: formContainer.current,
+            style: 'https://draggable.github.io/formeo/assets/css/formeo.min.css',
+            debug: true,
+          };
+          if (props.onSave) {
+            options.events = { onSave: props.onSave };
+          }
+          const renderer = new FormeoRenderer(options);
+          console.log('render: ', renderer);
+          setForm(renderer);
+          props.onLoad().then(res => {
+            console.log('res.data: ', res.data);
+            renderer.render(res.data);
+            console.log('rendered!');
+            return;
+          });
+        })
+        .catch(err => {
+          console.error('Error: ', err);
         });
-      })
-      .catch(err => {
-        console.error('Error: ', err);
-      });
+    }
+
+      if (!form) { initializeForm({ setForm, formContainer }) };
   }, []);
 
   return (
     <div>
-      <form className="formeo-renderer" />
+      <form className="formeo-renderer" ref={el => (formContainer.current = el)} />
     </div>
   );
 };
