6c7fc2069a406545dfc4f8d2033f18123b0a5704
Rae <rae.rudie@gmail.com>
2020-06-30 18:16:39 -0400

    complete color integration in dashboard

--- src/frontend/components/dashboard/SettingsTab.jsx
+++ src/frontend/components/dashboard/SettingsTab.jsx
@@ -1,7 +1,7 @@
 // base imports
-import React, { useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
-import _ from 'lodash/core';
 
 // bootstrap imports
 import Button from 'react-bootstrap/Button';
@@ -11,21 +11,23 @@ import Form from 'react-bootstrap/Form';
 // custom styles
 import './SettingsTab.css';
 
-const defaultColorPrimary = '#333333';
-const defaultColorSecondary = '#007bff';
-
-export default function SettingsTab() {
+export default function SettingsTab(props) {
+  const { defaults, setDefaults } = props;
   const [inputs, setInputs] = React.useState({});
-  const [colorPrimary, setColorPrimary] = React.useState(defaultColorPrimary);
-  const [colorSecondary, setColorSecondary] = React.useState(defaultColorSecondary);
 
   // color picker
   const handleChangeColorPrimary = color => {
-    setColorPrimary(color.hex);
+    setInputs(inputs => ({
+      ...inputs,
+      color_one: color.hex,
+    }));
   };
 
   const handleChangeColorSecondary = color => {
-    setColorSecondary(color.hex);
+    setInputs(inputs => ({
+      ...inputs,
+      color_two: color.hex,
+    }));
   };
 
   // other inputs
@@ -50,8 +52,8 @@ export default function SettingsTab() {
       title: inputs.title,
       header: inputs.header,
       footer: inputs.footer,
-      color_one: colorPrimary,
-      color_two: colorSecondary,
+      color_one: inputs.color_one,
+      color_two: inputs.color_two,
     });
     fetch('/api/v1/settings', {
       method: 'PUT',
@@ -66,8 +68,8 @@ export default function SettingsTab() {
       })
       .then(data => {
         if (status === 200 || status === 201) {
-          alert('Settings saved successfully.')
-          return data;
+          alert('Settings saved successfully.');
+          return setDefaults(inputs);
         } else {
           const error = processError(data);
           throw new Error(`Error in response from server: ${error}`);
@@ -79,49 +81,6 @@ export default function SettingsTab() {
       });
   };
 
-  const downloadSettings = () => {
-    let status;
-    return fetch('/api/v1/settings', {
-      method: 'GET',
-    })
-      .then(response => {
-        status = response.status;
-        return response.json();
-      })
-      .then(result => {
-        if (status === 200 || status === 201) {
-          if (!_.isEmpty(result.data)) {
-            setInputs(result.data);
-            setColorPrimary(result.data.color_one);
-            setColorSecondary(result.data.color_two);
-          }
-          return result.data;
-        } else {
-          const error = processError(result);
-          throw new Error(`Error in response from server: ${error}`);
-        }
-      })
-      .catch(error => {
-        console.error('error:', error);
-        throw Error(error.statusText);
-      });
-  };
-
-  useEffect(() => {
-    downloadSettings()
-      .then(data => {
-        if (!_.isEmpty(data)) {
-          setInputs(data);
-          setColorPrimary(data.color_one);
-          setColorSecondary(data.color_two);
-        }
-        return;
-      })
-      .catch(error => {
-        console.error('error:', error);
-      });
-  }, []);
-
   return (
     <Container className={'mt-2'}>
       <Form onSubmit={uploadSettings}>
@@ -132,7 +91,7 @@ export default function SettingsTab() {
             type="text"
             name="title"
             placeholder="Enter a title for the site"
-            defaultValue={inputs.title || ''}
+            defaultValue={inputs.title || defaults.title || ''}
             onChange={handleInputChange}
           />
         </Form.Group>
@@ -144,7 +103,7 @@ export default function SettingsTab() {
             rows="3"
             name="header"
             placeholder="Welcome text shown when first visiting te site"
-            defaultValue={inputs.header || ''}
+            defaultValue={inputs.header || defaults.header || ''}
             onChange={handleInputChange}
           />
         </Form.Group>
@@ -156,7 +115,7 @@ export default function SettingsTab() {
             rows="3"
             name="footer"
             placeholder="Text shown after taking the survey"
-            defaultValue={inputs.footer || ''}
+            defaultValue={inputs.footer || defaults.footer || ''}
             onChange={handleInputChange}
           />
         </Form.Group>
@@ -166,7 +125,7 @@ export default function SettingsTab() {
               Choose a primary default color for the site:
             </Form.Label>
             <SketchPicker
-              color={colorPrimary}
+              color={inputs.color_one || defaults.color_one}
               onChangeComplete={handleChangeColorPrimary}
             />
           </div>
@@ -175,7 +134,7 @@ export default function SettingsTab() {
               Choose a secondary default color for the site:
             </Form.Label>
             <SketchPicker
-              color={colorSecondary}
+              color={inputs.color_two || defaults.color_two}
               onChangeComplete={handleChangeColorSecondary}
             />
           </div>
@@ -185,3 +144,8 @@ export default function SettingsTab() {
     </Container>
   );
 }
+
+SettingsTab.propTypes = {
+  defaults: PropTypes.object.isRequired,
+  setDefaults: PropTypes.func.isRequired,
+};
