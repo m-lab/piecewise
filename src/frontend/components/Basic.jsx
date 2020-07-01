d4e5368d1166a2878f1b3c1650f8049b73db718a
Josh King <jking@chambana.net>
2020-07-01 14:49:17 -0400

    Switch frontpage to bootstrap and drill props.

--- src/frontend/components/Basic.jsx
+++ src/frontend/components/Basic.jsx
@@ -1,383 +1,204 @@
 // base imports
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 
-// material-ui imports
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Checkbox from '@material-ui/core/Checkbox';
-import Container from '@material-ui/core/Container';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormLabel from '@material-ui/core/FormLabel';
-import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
-import Paper from '@material-ui/core/Paper';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Typography from '@material-ui/core/Typography';
+// bootstrap imports
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 // module imports
 import ChromeScreengrab from '../assets/images/chrome-location.jpg';
 import FirefoxScreengrab from '../assets/images/firefox-location.jpg';
-import NDTjs from '../assets/js/ndt-browser-client.js';
-import ThanksDialog from './utils/ThanksDialog.jsx';
 
-const useStyles = makeStyles(theme => ({
-  input: {
-    display: 'none',
-  },
-  h1: {
-    fontFamily: 'Poppins',
-    fontSize: '26px',
-    lineHeight: '32px',
-    fontWeight: '700',
-  },
-  h6: {
-    fontFamily: 'Poppins',
-    fontSize: '16px',
-    lineHeight: '20px',
-    fontWeight: '700',
-    color: '#4A4A4A',
-  },
-  debug: {
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    lineHeight: '16px',
-    color: '#4A4A4A',
-    fontFamily: 'monospace',
-  },
-  FormControlLabel: {
-    marginBottom: '5px',
-  },
-  media: {
-    minHeight: '200px',
-  },
-  sub1: {
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    lineHeight: '16px',
-    color: '#4A4A4A',
-  },
-  sub1a: {
-    marginTop: theme.spacing(1),
-    fontSize: '14px',
-    lineHeight: '16px',
-    color: '#4A4A4A',
-  },
-  paper: {
-    padding: theme.spacing(4),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  centerText: {
-    textAlign: 'center',
-  },
-}));
-
-export default function Basic(props) {
-  const classes = useStyles();
-
+export default function Basic() {
+  const history = useHistory();
   // handle geolocation consent
-  const [locationValue, setLocationValue] = React.useState('yes');
+  const [locationConsent, setLocationConsent] = useState(false);
+  // site settings
+  const [settings, setSettings] = useState({});
 
-  const handleLocationChange = event => {
-    setLocationValue(event.target.value);
+  const processError = errorMessage => {
+    let text = `We're sorry your, request didn't go through. Please send the message below to the support team and we'll try to fix things as soon as we can.`;
+    let debug = JSON.stringify(errorMessage);
+    return [text, debug];
   };
 
-  // handle mlab privacy consent
-  const [consentState, setConsentState] = React.useState({ checked: false });
+  // fetch settings from API
+  const downloadSettings = () => {
+    let status;
+    return fetch('/api/v1/settings', {
+      method: 'GET',
+    })
+      .then(response => {
+        status = response.status;
+        return response.json();
+      })
+      .then(result => {
+        if (status === 200 || status === 201) {
+          if (result.data) {
+            return result.data;
+          } else {
+            const error = processError(result);
+            throw new Error(`Error in response from server: ${error}`);
+          }
+        } else {
+          const error = processError(result);
+          throw new Error(`Error in response from server: ${error}`);
+        }
+      })
+      .catch(error => {
+        console.error('error:', error);
+        throw Error(error.statusText);
+      });
+  };
 
-  const handleConsentChange = event => {
-    setConsentState({
-      ...consentState,
-      [event.target.name]: event.target.checked,
+  useEffect(() => {
+    downloadSettings()
+      .then(data => {
+        if (data) {
+          setSettings(data);
+        }
+        return;
+      })
+      .catch(error => {
+        console.error('error:', error);
+        throw Error(error.statusText);
+      });
+  }, []);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    history.push({
+      pathname: '/survey',
+      state: { settings: settings, locationConsent: locationConsent },
     });
   };
 
-  const consentError = consentState.checked !== true;
+  //function success(position) {
+  //  document.getElementById('latitude-mlab').value = position.coords.latitude;
+  //  document.getElementById('longitude-mlab').value = position.coords.longitude;
+  //  document.getElementById('latitude').value = position.coords.latitude;
+  //  document.getElementById('longitude').value = position.coords.longitude;
 
-  // handle form dialog open
-  const [open, setOpen] = React.useState(false);
+  //  var xhr = new XMLHttpRequest(),
+  //    currentLocationURL =
+  //      'https://nominatim.openstreetmap.org/reverse?format=json&lat=' +
+  //      position.coords.latitude +
+  //      '&lon=' +
+  //      position.coords.longitude +
+  //      '&zoom=18&addressdetails=1';
 
-  const handleClickOpen = () => {
-    if (!consentError) {
-      setOpen(true);
-      //if (locationValue === 'yes') {
-      //  if ('geolocation' in navigator) {
-      //    navigator.geolocation.getCurrentPosition(success, error);
-      //  }
-      //} else {
-      //  runTests();
-      //}
-    }
-  };
+  //  var currentLoc;
+  //  xhr.open('GET', currentLocationURL, true);
+  //  xhr.send();
+  //  xhr.onreadystatechange = function() {
+  //    if (xhr.readyState === 4) {
+  //      if (xhr.status === 200) {
+  //        currentLoc = JSON.parse(xhr.responseText);
+  //        console.log('Location received');
+  //        // currentLocText.text(currentLoc.address.road + currentLoc.address.neighbourhood + currentLoc.address.suburb + currentLoc.address.city + currentLoc.address.state);
+  //        document
+  //          .getElementsByClassName('loader-item')[1]
+  //          .append(
+  //            'Searching from: ' +
+  //              currentLoc.address.road +
+  //              ', ' +
+  //              currentLoc.address.city +
+  //              ', ' +
+  //              currentLoc.address.state,
+  //          );
+  //      } else {
+  //        console.log('Location lookup failed');
+  //      }
+  //    }
+  //  };
+  //}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  // handle NDT test
-  const [ndtServer, setNdtServer] = React.useState(null);
-  const [ndtServerIp, setNdtServerIp] = React.useState(null);
-
-  let ndtPort = '3010',
-    ndtProtocol = 'wss',
-    ndtPath = '/ndt_protocol',
-    ndtUpdateInterval = 1000,
-    c2sRate,
-    s2cRate,
-    MinRTT;
-
-  function success(position) {
-    const NDT_client = new NDTjs(
-      ndtServer,
-      ndtPort,
-      ndtProtocol,
-      ndtPath,
-      undefined,
-      ndtUpdateInterval,
-    );
-
-    document.getElementById('latitude-mlab').value = position.coords.latitude;
-    document.getElementById('longitude-mlab').value = position.coords.longitude;
-    document.getElementById('latitude').value = position.coords.latitude;
-    document.getElementById('longitude').value = position.coords.longitude;
-
-    var xhr = new XMLHttpRequest(),
-      currentLocationURL =
-        'https://nominatim.openstreetmap.org/reverse?format=json&lat=' +
-        position.coords.latitude +
-        '&lon=' +
-        position.coords.longitude +
-        '&zoom=18&addressdetails=1';
-
-    var currentLoc;
-    xhr.open('GET', currentLocationURL, true);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          currentLoc = JSON.parse(xhr.responseText);
-          console.log('Location received');
-          NDT_client.startTest();
-          // currentLocText.text(currentLoc.address.road + currentLoc.address.neighbourhood + currentLoc.address.suburb + currentLoc.address.city + currentLoc.address.state);
-          document
-            .getElementsByClassName('loader-item')[1]
-            .append(
-              'Searching from: ' +
-                currentLoc.address.road +
-                ', ' +
-                currentLoc.address.city +
-                ', ' +
-                currentLoc.address.state,
-            );
-        } else {
-          console.log('Location lookup failed');
-        }
-      }
-    };
-  }
-
-  function error(error) {
-    window.alert(error.message);
-  }
-
-  function getNdtServer() {
-    if (!ndtServer) {
-      const xhr = new XMLHttpRequest(),
-        mlabNsUrl = 'https://mlab-ns.appspot.com/ndt_ssl?format=json';
-
-      xhr.open('GET', mlabNsUrl, true);
-      xhr.send();
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            setNdtServer(JSON.parse(xhr.responseText).fqdn);
-            setNdtServerIp(JSON.parse(xhr.responseText).ip);
-          } else {
-            console.log('M-Lab NS lookup failed.');
-            window.alert('M-Lab NS lookup failed. Please refresh the page.');
-          }
-        }
-      };
-    }
-  }
-
-  function runTests(event) {
-    const NDT_client = new NDTjs(
-      ndtServer,
-      ndtPort,
-      ndtProtocol,
-      ndtPath,
-      undefined,
-      ndtUpdateInterval,
-    );
-
-    NDT_client.startTest();
-  }
-
-  React.useEffect(() => {
-    getNdtServer();
-    console.log('Using M-Lab Server ' + ndtServer);
-  }, [ndtServer]);
-
-  return (
-    <Container maxWidth="lg">
-      <Paper className={classes.paper} elevation={0}>
-        <Box mb={3}>
-          <Typography
-            className={classes.h1}
-            color="primary"
-            variant="h4"
-            component="h1"
-          >
-            Piecewise Broadband Speed Test
-          </Typography>
-          <Typography
-            className={classes.sub1a}
-            variant="subtitle1"
-            component="p"
-            gutterBottom
-          >
-            Sample subtitle
-          </Typography>
-          <Typography
-            className={classes.h2}
-            color="primary"
-            variant="h5"
-            component="h2"
-          >
-            Sharing your location
-          </Typography>
-          <Typography
-            className={classes.body1}
-            variant="body1"
-            component="p"
-            gutterBottom
-          >
-            To get the most accurate location data, we ask you to allow your
-            browser to share your location. This is not essential but it is very
-            helpful for creating more accurate maps. Depending on your browser,
-            you'll see a window similar to the images below, requesting your
-            consent to share your location. If you are using Private Browsing
-            mode or Incognito mode, you may need to disable that preference for
-            this website.
-          </Typography>
-        </Box>
-        <Box mb={3}>
-          <Grid container spacing={2} justify="center">
-            <Grid item>
-              <Card>
-                <CardMedia
-                  className={classes.media}
-                  image={FirefoxScreengrab}
-                  title="Screenshot of geography location request in Firefox."
-                />
-                <CardContent>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Screenshot of geography location request in Firefox.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item>
-              <Card>
-                <CardMedia
-                  className={classes.media}
-                  image={ChromeScreengrab}
-                  title="Screenshot of geography location request in Chrome."
-                />
-                <CardContent>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Screenshot of geography location request in Chrome.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        </Box>
-        <Box mb={3}>
-          <FormControl component="fieldset">
-            <Hidden>
-              <FormLabel component="legend">
-                Do you want to use your browser location?
-              </FormLabel>
-            </Hidden>
-            <RadioGroup
-              aria-label="location-choice"
-              name="location"
-              value={locationValue}
-              onChange={handleLocationChange}
-            >
-              <FormControlLabel
-                value="yes"
-                control={<Radio />}
-                label="Use my browser location"
-                className={classes.FormControlLabel}
+  if (!settings) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Container fluid="lg">
+        <h1>{settings.title}</h1>
+        <p>{settings.header}</p>
+        <h2>Sharing your location</h2>
+        <p>
+          To get the most accurate location data, we ask you to allow your
+          browser to share your location. This is not essential but it is very
+          helpful for creating more accurate maps. Depending on your browser,
+          you&lsquoll see a window similar to the images below, requesting your
+          consent to share your location. If you are using Private Browsing mode
+          or Incognito mode, you may need to disable that preference for this
+          website.
+        </p>
+        <Container>
+          <Row>
+            <Card>
+              <Card.Img
+                src={FirefoxScreengrab}
+                alt="Screenshot of geography location request in Firefox."
               />
-              <FormControlLabel
-                value="no"
-                control={<Radio />}
-                label="Do not use my location"
-                className={classes.FormControlLabel}
-                name="useBrowserLocation"
+              <Card.Text>
+                Screenshot of geography location request in Firefox.
+              </Card.Text>
+            </Card>
+          </Row>
+          <Row>
+            <Card>
+              <Card.Img
+                src={ChromeScreengrab}
+                alt="Screenshot of geography location request in Chrome."
               />
-            </RadioGroup>
-          </FormControl>
-          <div>
-            <FormControl required error={consentError}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={consentState.checked}
-                    onChange={handleConsentChange}
-                    name="checked"
-                    color="primary"
-                  />
-                }
+              <Card.Text>
+                Screenshot of geography location request in Chrome.
+              </Card.Text>
+            </Card>
+          </Row>
+        </Container>
+        <Form onSubmit={handleSubmit}>
+          <Container>
+            <fieldset>
+              <Form.Group>
+                <Form.Label>
+                  Do you want to use your browser location?
+                </Form.Label>
+                <Form.Check
+                  type="radio"
+                  id="location-yes"
+                  label="Use my browser location"
+                  onChange={() => setLocationConsent(true)}
+                />
+                <Form.Check
+                  type="radio"
+                  id="location-no"
+                  label="Do not use my location"
+                  onChange={() => setLocationConsent(false)}
+                />
+              </Form.Group>
+            </fieldset>
+            <Form.Group>
+              <Form.Check
+                required
+                type="checkbox"
+                id="consent"
                 label="*I agree to the M-Lab privacy policy, which includes retention and publication of IP addresses, in addition to speed test results."
               />
-              <FormHelperText>This field is required</FormHelperText>
-            </FormControl>
-          </div>
-          <Box m={2} mx="auto" className={classes.centerText}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleClickOpen}
-            >
+              <Form.Text>This field is required</Form.Text>
+            </Form.Group>
+          </Container>
+          <Container>
+            <Button variant="primary" type="submit">
               Take the Test
             </Button>
-          </Box>
-        </Box>
-        <ThanksDialog open={open} onClose={handleClose} />
-      </Paper>
-      {/*
-      <MUICookieConsent
-        cookieName="piecewiseCookieConsent"
-        componentType="Snackbar" // default value is Snackbar
-        message="This site uses cookies.... bla bla..."
-      />
-      */}
-    </Container>
-  );
+          </Container>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Basic.propTypes = {
