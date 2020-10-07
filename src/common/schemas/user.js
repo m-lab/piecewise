3d4b4c42806ce403bd5f739abdb1c3b977036b54
Josh King <jking@chambana.net>
2020-10-07 16:54:47 -0400

    Allow null for optional user fields in schema.

--- src/common/schemas/user.js
+++ src/common/schemas/user.js
@@ -4,17 +4,37 @@ import Joi from '@hapi/joi';
 const schema = Joi.object({
   id: Joi.number(),
   username: Joi.string(),
-  firstName: Joi.string().optional(),
-  lastName: Joi.string().optional(),
-  instance: Joi.string().optional(),
-  instance_name: Joi.string().optional(),
-  instance_domain: Joi.string().optional(),
-  role: Joi.number().optional(),
+  firstName: Joi.string()
+    .allow(null)
+    .optional(),
+  lastName: Joi.string()
+    .allow(null)
+    .optional(),
+  instance: Joi.integer()
+    .allow(null)
+    .optional(),
+  instance_name: Joi.string()
+    .allow(null)
+    .optional(),
+  instance_domain: Joi.string()
+    .allow(null)
+    .optional(),
+  role: Joi.number()
+    .allow(null)
+    .optional(),
   role_name: Joi.string(),
-  email: Joi.string().optional(),
-  phone: Joi.string().optional(),
-  extension: Joi.string().optional(),
-  isActive: Joi.number().optional(),
+  email: Joi.string()
+    .allow(null)
+    .optional(),
+  phone: Joi.string()
+    .allow(null)
+    .optional(),
+  extension: Joi.string()
+    .allow(null)
+    .optional(),
+  isActive: Joi.number()
+    .allow(null)
+    .optional(),
 });
 
 export async function validate(data) {
