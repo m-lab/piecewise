f380403dc42b9b70a868b2b661d70248222e1948
Jeremia Kimelman <jbkimelman@gmail.com>
2020-10-12 00:53:22 -0700

    Merge MLab data into admin vectortiles

---
+++ src/vectortiles/scripts/download-mlab.js
@@ -0,0 +1,49 @@
+const { exec: execWithCallback } = require('child_process');
+
+function exec(args) {
+  return new Promise((resolve, reject) => {
+    execWithCallback(args, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+}
+
+const { default: Queue } = require('p-queue');
+
+const args = process.argv;
+const geographicLevel = args[2] || 'counties'; // can also be "tracts"
+const rootGsUrl = `gs://statistics-mlab-sandbox/v0/NA/US/${geographicLevel}`;
+
+async function download(fips) {
+  const cmd = `gsutil cp ${rootGsUrl}/${fips}/histogram_daily_stats.json mlab/${geographicLevel}/${fips}.json`;
+  try {
+    console.log(`Downloading JSON file (FIPS: ${fips})`);
+    await exec(cmd);
+  } catch (e) {
+    console.error(`Error with ${fips}\n\t`, e);
+  }
+}
+
+async function main() {
+  const filesList = await exec(`gsutil ls ${rootGsUrl}/`);
+  const files = filesList.split('\n');
+
+  const queue = new Queue({ concurrency: 4 });
+  const fipsLength = geographicLevel === 'counties' ? '5' : '11';
+  const r = new RegExp(`${geographicLevel}\\/(\\d{${fipsLength}})\\/$`);
+
+  files.forEach(file => {
+    const match = file.match(r);
+    if (!match) return;
+
+    const fips = match[1];
+    queue.add(() => download(fips));
+  });
+
+  await queue.onIdle();
+
+  console.log(`Downloaded ${files.length} JSON files`);
+}
+
+main();
---
+++ src/vectortiles/scripts/process-mlab.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+const { nest } = require('d3');
+const glob = require('glob');
+const { default: Queue } = require('p-queue');
+
+const args = process.argv;
+const geographicLevel = args[2] || 'counties'; // can also be "tracts"
+
+glob(`mlab/${geographicLevel}/*.json`, async (err, files) => {
+  let isFirst = true;
+  const output = fs.createWriteStream(`mlab-${geographicLevel}.json`);
+  const queue = new Queue({ concurrency: 4 });
+
+  async function processFile(filePath) {
+    let delimiter = ',';
+    const fipsMatch = filePath.match(/(\d+)\.json$/);
+    const fips = fipsMatch ? fipsMatch[1] : null;
+    const f = await fs.promises.readFile(filePath);
+    const j = JSON.parse(f.toString());
+
+    const grouped = nest()
+      .key(d => {
+        const { date } = d;
+        const [year, month] = date.split('-');
+        const halfOfTheYear = ['01', '02', '03', '04', '05', '06'].includes(
+          month,
+        )
+          ? 'jan_jun'
+          : 'july_dec';
+
+        return `${year}_${halfOfTheYear}`;
+      })
+      .entries(j);
+
+    // currently only looks at download speeds
+    const analyzed = grouped.map(grouped => {
+      const { key, values } = grouped;
+
+      let totalDlSamples = 0;
+      let dlSamplesOverAudio = 0;
+      let dlSamplesOverVideo = 0;
+
+      values.forEach(v => {
+        const { bucket_min, dl_samples } = v;
+
+        totalDlSamples += dl_samples;
+
+        if (bucket_min > 2.5) {
+          dlSamplesOverAudio += dl_samples;
+        }
+
+        if (bucket_min > 10) {
+          dlSamplesOverVideo += dl_samples;
+        }
+      });
+
+      return {
+        [`${key}_total_dl_samples`]: totalDlSamples,
+        [`${key}_percent_over_audio_threshold`]: dlSamplesOverAudio / totalDlSamples,
+        [`${key}_percent_over_video_threshold`]: dlSamplesOverVideo / totalDlSamples,
+      };
+    });
+
+    const d = { geo_id: fips };
+    analyzed.forEach(a => {
+      Object.assign(d, a);
+    });
+
+    if (isFirst) delimiter = '';
+    isFirst = false;
+
+    output.write(`${delimiter}\n${JSON.stringify(d)}`);
+    console.log(`Processed ${filePath}`);
+  }
+
+  output.write('[');
+  files.forEach(file => {
+    queue.add(() => processFile(file));
+  });
+
+  await queue.onIdle();
+  output.write(']');
+
+  console.log(`Processed data from ${files.length} source files`);
+});
