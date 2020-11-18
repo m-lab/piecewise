01ae7f96be0d9d0dbbe058987e1cac6b9f68c471
Jeremia Kimelman <jbkimelman@gmail.com>
2020-11-18 12:43:00 -0800

    Update Makefile to account for tract volume & Census

--- src/vectortiles/scripts/download-mlab.js
+++ src/vectortiles/scripts/download-mlab.js
@@ -1,24 +1,26 @@
 const { exec: execWithCallback } = require('child_process');
 
+const { default: Queue } = require('p-queue');
+
 function exec(args) {
   return new Promise((resolve, reject) => {
-    execWithCallback(args, (err, data) => {
+    execWithCallback(args, { maxBuffer: 2000 * 1024 }, (err, data) => {
       if (err) return reject(err);
       resolve(data);
     });
   });
 }
 
-const { default: Queue } = require('p-queue');
-
 const args = process.argv;
 const geographicLevel = args[2] || 'counties'; // can also be "tracts"
 const rootGsUrl = `gs://statistics-mlab-sandbox/v0/NA/US/${geographicLevel}`;
 
-async function download(fips) {
+let fileCount = 0;
+
+async function download(fips, i) {
   const cmd = `gsutil cp ${rootGsUrl}/${fips}/2020/histogram_daily_stats.json mlab/${geographicLevel}/${fips}.json`;
   try {
-    console.log(`Downloading JSON file (FIPS: ${fips})`);
+    console.log(`Downloading JSON file (FIPS: ${fips}, ${i} / ${fileCount})`);
     await exec(cmd);
   } catch (e) {
     console.error(`Error with ${fips}\n\t`, e);
@@ -26,19 +28,25 @@ async function download(fips) {
 }
 
 async function main() {
+  console.log(`Finding MLab data files to download for ${geographicLevel}`);
   const filesList = await exec(`gsutil ls ${rootGsUrl}/`);
   const files = filesList.split('\n');
+  fileCount = files.length;
 
-  const queue = new Queue({ concurrency: 4 });
+  console.log(`Found ${fileCount} files to download, starting now`);
+
+  const queue = new Queue({ concurrency: 16 });
   const fipsLength = geographicLevel === 'counties' ? '5' : '11';
   const r = new RegExp(`${geographicLevel}\\/(\\d{${fipsLength}})\\/$`);
 
-  files.forEach(file => {
+  files.forEach((file, fileIndex) => {
     const match = file.match(r);
     if (!match) return;
 
     const fips = match[1];
-    queue.add(() => download(fips));
+    queue.add(async () => {
+      await download(fips, fileIndex);
+    });
   });
 
   await queue.onIdle();
